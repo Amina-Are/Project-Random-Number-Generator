aa7a3802f8e7de9e3c9bc4e4e7ce03ac8ec49c19
Amina-Are <amina_aremu@hotmail.co.uk>
2024-07-22 00:20:12 +0100

    Add files via upload

---
+++ Project- random number generator.py
@@ -0,0 +1,13 @@
+
+import random
+
+print(random.randrange(1,10))
+
+#defining multi-word variables
+myFathersName = "thisIsCamelCase"
+MyFathersName = "ThisIsPascalCase"
+my_fathers_name ="this_is_snake_case"
+
+
+name = "Amina"
+print (name)
